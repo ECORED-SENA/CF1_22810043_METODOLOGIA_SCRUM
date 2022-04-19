19ba256d98319125bc7a5e54918b966d444d0f03
Va-Gomez <99420453+Va-Gomez@users.noreply.github.com>
2022-04-19 12:30:38 -0500

    Actualizando_tem4

--- src/views/Tema4.vue
+++ src/views/Tema4.vue
@@ -7,6 +7,15 @@
         span 4
       h1  Artefactos  
 
+    
+    .row.p-4.mb-5.fondo-8.align-items-center.justify-content-center
+      .col-lg-6
+        figure
+          img(src='@/assets/curso/tema4/img1.png' alt='')
+      .col-lg-6
+        p.mb-4 La idea de las premisas en Scrum es que la satisfacción del producto, no sea solo parte del cliente, sino que también debe ser para cada uno de los integrantes del equipo ejecutor. 
+        span Como parte de la metodología, las premisas favorecen la comprensión conceptual de la misma y dan soporte argumental a su propuesta; ellas están directamente relacionadas con los valores Scrum, y son las máximas orientaciones para la efectividad de la aplicación de esta metodología.
+    separador
       
 
 </template>
