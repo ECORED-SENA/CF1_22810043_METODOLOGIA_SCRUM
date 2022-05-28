fbb2afdfe6729eae83dcd40ff56ad20d4024fcda
Va-Gomez <99420453+Va-Gomez@users.noreply.github.com>
2022-05-27 19:54:08 -0500

    Actualizado

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -13,9 +13,9 @@
       p.mb-0 Le damos la bienvenida al componente <font color="#0043FF"><b>“Scrum, descripciones y características”</b>.</font> Para comenzar, visualice el recurso que, a continuación, se muestra. <font color="#0043FF"><b>¡Adelante!</b></font>
     figure.mb-5(data-aos="zoom-in-up")
       .video
-        iframe(width="942" height="530" src="https://www.youtube.com/embed/bcLlezgaqic" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+        iframe(width="942" height="530" src="https://www.youtube.com/embed/8YD5lTDzhcY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
       
- 
+
 </template>
 
 <script>
